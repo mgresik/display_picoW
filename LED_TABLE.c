e2a578632ebbf90398a7a202bdc775980f950c84
mgresik <pwlove00@gmail.com>
2026-06-26 14:53:16 +0300

    to do

--- LED_TABLE.c
+++ LED_TABLE.c
@@ -88,5 +88,6 @@ int main()
     while (true) {
         printf("Hello, world!\n");
         sleep_ms(1000);
+        // Make a run snake*************
     }
 }
--- readme.md
+++ readme.md
@@ -1,2 +1,6 @@
 GP8 - SDA (I2C)
 GP9 - SCL (I2C)
+
+TO DO:
+- make a snake;
+- make a downloading screen
